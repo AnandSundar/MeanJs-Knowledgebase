10870f2c920ff0022605215b2e51cf1fcfc3d5e2
AnandSundar <anandsundar96@gmail.com>
2017-12-03 21:05:44 -0500

    created and configured the models

---
+++ models/article.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+
+var articleSchema = mongoose.Schema({
+  title: {
+    type: String,
+    index: true,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    index: true,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+//module exports allows the Article object creates to be accessed from anywhere
+var Article = module.exports = mongoose.model('Article', articleSchema);
+
+//Get all articles
+module.exports.getArticles = function(callback){
+  Article.find(callback); //mongoose command
+}
+
+// Get article by Id
+module.exports.getArticleById = function(id,callback) {
+  Article.findById(id,callback);
+}
+
+//Get category articles
+module.exports.getArticleByCategory = function(category, callback){
+  var query = {category: category}
+  Article.find(query,callback);
+}
+
+//
